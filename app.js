decdbff213426ac4ec16ba5ed0f97c2c4e220a08
Maximinsavi <toessijeannette69@gmail.com>
2025-10-21 04:57:53 +0100

    Update app.js

--- app.js
+++ app.js
@@ -169,7 +169,7 @@ form.addEventListener("submit", async (event) => {
   setLoading(true);
 
   try {
-    const response = await fetch("//gemini-web-api.onrender.com/gemini", {
+    const response = await fetch("https://rest-apiv2-mnri.onrender.com/api/gptChat", {
       method: "POST",
       body: formData,
     });
